967aa0e728381be4c9e2964e9ce24912ad162974
anselmo <anselmostitla@gmail.com>
2025-04-05 10:49:08 -0600

    Just doing some manual analysis

--- 4-thunder/src/interfaces/IThunderLoan.sol
+++ 4-thunder/src/interfaces/IThunderLoan.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+// @audit-info the IThunderLoan should be implemented by the ThunderLoan contract
+
 interface IThunderLoan {
+    // @audit low/informational ?? is address token or ERC20 token
     function repay(address token, uint256 amount) external;
 }
